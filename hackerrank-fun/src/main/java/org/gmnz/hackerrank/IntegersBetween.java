0e16185cc3fd0bd6b0a814999772dd228594560a
S3lvatico <s3lvatico@gmail.com>
2019-04-20 15:57:36 +0200

    trovo i numeri primi, da solo

---
+++ hackerrank-fun/src/main/java/org/gmnz/hackerrank/IntegersBetween.java
@@ -0,0 +1,20 @@
+package org.gmnz.hackerrank;
+
+public class IntegersBetween {
+
+    /*
+     * gli interi di mezzo tra due insiemi sono quelli per i quali:
+     * 
+     * dati due insiemi di interi positivi ogni elemento del primo insieme divide
+     * l'intero di mezzo l'intero di mezzo divide ogni elemento del secondo insieme
+     */
+
+    /*
+     * 
+     * [ 2 4 ] [ 16 32 96]
+     * 
+     * 2^1, 2^2 2^4, 2^5, 2^5*3
+     * 
+     * 
+     */
+}
---
+++ hackerrank-fun/src/main/java/org/gmnz/hackerrank/PrimeNumbers.java
@@ -0,0 +1,71 @@
+package org.gmnz.hackerrank;
+
+import java.util.Arrays;
+
+/**
+ * PrimeNumbers
+ */
+public class PrimeNumbers {
+
+    private int[] findPrimes(int upperBound) {
+        boolean[] sieve = new boolean[upperBound + 1];
+        Arrays.fill(sieve, true);
+        for (int i = 2; i < sieve.length / 2; i++) {
+            if (!sieve[i]) {
+                continue;
+            }
+            for (int n = i; n <= upperBound; n += i) {
+                if (n == i)
+                    continue;
+                sieve[n] = false;
+            }
+        }
+        // già a questo punto sieve[] contiene un true nell'indice che corrisponde a un
+        // numero primo (eccezion fatta per gli indici 0 e 1 che in questo contesto non
+        // hanno significato)
+        int primeCount = 0;
+        for (int i = 2; i < sieve.length; i++) {
+            if (sieve[i]) {
+                primeCount++;
+            }
+        }
+
+        int[] primeNumbers = new int[primeCount];
+        int pidx = 0;
+        for (int i = 2; i < sieve.length; i++) {
+            if (sieve[i]) {
+                primeNumbers[pidx] = i;
+                pidx++;
+            }
+        }
+
+        return primeNumbers;
+    }
+
+    void printPrimes(int howMany) {
+        int[] primes = new int[howMany];
+        primes[0] = 2;
+        for (int i = 1; i < primes.length; i++) {
+            int n = primes[i - 1];
+        }
+    }
+
+    private static void prinIntArray(int[] integers) {
+        StringBuilder sb = new StringBuilder("[  ");
+        for (int i : integers) {
+            sb.append(String.format("%-4d", i));
+        }
+        sb.append("]");
+        System.out.println(sb.toString());
+    }
+
+    public static void main(String[] args) {
+        final int N_PRIMES = 7;
+        PrimeNumbers pn = new PrimeNumbers();
+        pn.printPrimes(N_PRIMES);
+        System.out.println("bubu");
+        int[] primeNumbers = pn.findPrimes(100);
+        prinIntArray(primeNumbers);
+    }
+
+}
