6c789c7285763c766db15a57ced579322fbf31a8
S3lvatico <s3lvatico@gmail.com>
2019-04-20 20:43:02 +0200

    trovato algoritmo per il mcm

--- hackerrank-fun/src/main/java/org/gmnz/hackerrank/Utils.java
+++ hackerrank-fun/src/main/java/org/gmnz/hackerrank/Utils.java
@@ -15,6 +15,19 @@ class Utils {
         return max;
     }
 
+    static int mcm(int a, int b) {
+        int min = a <= b ? a : b;
+        int max = a > b ? a : b;
+        int x = 0;
+        for (int i = 1; i <= min; i++) {
+            x = max * i;
+            if (x % min == 0) {
+                return x;
+            }
+        }
+        return -1;
+    }
+
     static void printArray(int[] integers) {
         int maxValue = findMax(integers);
         double log10Max = Math.log10(maxValue);
