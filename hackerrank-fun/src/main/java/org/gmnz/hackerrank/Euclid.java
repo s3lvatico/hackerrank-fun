9801e5507d7c3e7ca2a1edaffca5d60106c58059
S3lvatico <s3lvatico@gmail.com>
2019-04-21 21:59:28 +0200

    correzione dipendenza log4j2

--- hackerrank-fun/src/main/java/org/gmnz/hackerrank/Euclid.java
+++ hackerrank-fun/src/main/java/org/gmnz/hackerrank/Euclid.java
@@ -1,7 +1,9 @@
 package org.gmnz.hackerrank;
 
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
 
 /**
  * L'algoritmo euclideo.
@@ -10,52 +12,60 @@ import org.apache.logging.log4j.Logger;
  */
 public class Euclid {
 
-    private static final Logger logger = LogManager.getLogger("HelloWorld");
+	private static final Logger logger = LogManager.getLogger("HelloWorld");
 
-    /**
-     * Massimo comune divisore.
-     * <p>
-     * L'algoritmo prosegue finché <code>r_k > 0</code>
-     * <p>
-     * <code>r_(k-2) = q_k * r_(k-1) + r_k</code>
-     * <p>
-     * E per <code>k = 0</code> si pone <code>r_(k-2) = a</code> e
-     * <code>r_(k-1) = b</code>
-     * <p>
-     * Ad ogni passaggio ciò che occorre davvero determinare è il resto r_k
-     */
-    static long gcd(long a, long b) {
-        if (a < b) {
-            return gcd(b, a);
-        }
-        while (b > 0) {
-            long temp = b;
-            b = a % b; // % is remainder
-            a = temp;
-        }
-        return a;
-    }
 
-    static long gcd(long[] input) {
-        long result = input[0];
-        for (int i = 1; i < input.length; i++)
-            result = gcd(result, input[i]);
-        return result;
-    }
 
-    /**
-     * 
-     * https://en.wikipedia.org/wiki/Least_common_multiple#Reduction_by_the_greatest_common_divisor
-     */
-    static long lcm(long a, long b) {
-        return a * (b / gcd(a, b));
-    }
+	/**
+	 * Massimo comune divisore.
+	 * <p>
+	 * L'algoritmo prosegue finché <code>r_k > 0</code>
+	 * <p>
+	 * <code>r_(k-2) = q_k * r_(k-1) + r_k</code>
+	 * <p>
+	 * E per <code>k = 0</code> si pone <code>r_(k-2) = a</code> e
+	 * <code>r_(k-1) = b</code>
+	 * <p>
+	 * Ad ogni passaggio ciò che occorre davvero determinare è il resto r_k
+	 */
+	static long gcd(long a, long b) {
+		if (a < b) {
+			return gcd(b, a);
+		}
+		while (b > 0) {
+			long temp = b;
+			b = a % b; // % is remainder
+			a = temp;
+		}
+		return a;
+	}
 
-    static long lcm(long[] input) {
-        long result = input[0];
-        for (int i = 1; i < input.length; i++)
-            result = lcm(result, input[i]);
-        return result;
-    }
+
+
+	static long gcd(long[] input) {
+		long result = input[0];
+		for (int i = 1; i < input.length; i++)
+			result = gcd(result, input[i]);
+		return result;
+	}
+
+
+
+	/**
+	 * 
+	 * https://en.wikipedia.org/wiki/Least_common_multiple#Reduction_by_the_greatest_common_divisor
+	 */
+	static long lcm(long a, long b) {
+		return a * (b / gcd(a, b));
+	}
+
+
+
+	static long lcm(long[] input) {
+		long result = input[0];
+		for (int i = 1; i < input.length; i++)
+			result = lcm(result, input[i]);
+		return result;
+	}
 
 }
