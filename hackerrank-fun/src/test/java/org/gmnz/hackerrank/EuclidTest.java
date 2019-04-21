bcb07984b8cbe651a705fb3524474de8a193ef64
S3lvatico <s3lvatico@gmail.com>
2019-04-21 20:38:42 +0200

    scritto test per algoritmo euclideo

---
+++ hackerrank-fun/src/test/java/org/gmnz/hackerrank/EuclidTest.java
@@ -0,0 +1,15 @@
+package org.gmnz.hackerrank;
+
+import org.junit.Test;
+
+/**
+ * EuclidTest
+ */
+public class EuclidTest {
+
+    @Test
+    public void testMcd() {
+        long l = Euclid.gcd(12, 8);
+    }
+
+}
