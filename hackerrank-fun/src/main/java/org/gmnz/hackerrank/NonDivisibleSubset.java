372f0af14a45bb71606bb73abe812a4d80754c9c
S3lvatico <s3lvatico@gmail.com>
2019-04-29 22:22:43 +0200

    mezza roba ma ci devo rifletter meglio

--- hackerrank-fun/src/main/java/org/gmnz/hackerrank/NonDivisibleSubset.java
+++ hackerrank-fun/src/main/java/org/gmnz/hackerrank/NonDivisibleSubset.java
@@ -11,127 +11,72 @@ import java.util.Set;
  */
 public class NonDivisibleSubset {
 
-    static class Factorial {
+    static class Nds {
 
-        private Map<Integer, Long> fMap = new HashMap<>();
+        private final int k;
+        private final Set<Integer> _set;
 
-        {
-            fMap.put(0, 1L);
-            fMap.put(1, 1L);
+        Nds(int k) {
+            this.k = k;
+            _set = new HashSet<>();
         }
 
-        long of(int n) {
-            if (!fMap.containsKey(n)) {
-                fMap.put(n, n * of(n - 1));
-            }
-            return fMap.get(n);
-        }
-
-    }
-
-    static class Combinatorics {
-
-        long dispositions(int n, int k) {
-            Factorial fact = new Factorial();
-            return fact.of(n) / fact.of(n - k);
-        }
-
-        long combinations(int n, int k) {
-            Factorial fact = new Factorial();
-            long dispositions = dispositions(n, k);
-            return dispositions / fact.of(k);
-        }
-    }
-
-    /*
-    
-    
-    */
-    int nonDivisibleSubset(int k, int[] S) {
-        int n = S.length;
-        boolean[] sieve = new boolean[n];
-        Arrays.fill(sieve, Boolean.TRUE);
-
-        /* doppio ciclo for per generare tutte le combinazioni */
-        for (int i = 0; i < n - 1; i++) {
-            int sum;
-            for (int j = i + 1; j < n; j++) {
-                if (sieve[i] && sieve[j]) {
-                    sum = S[i] + S[j];
-                    sieve[j] = (sum % k) != 0;
+        boolean canBeAdded(int n) {
+            if (_set.size() == 0) {
+                return true;
+            } else {
+                int sum;
+                for (int s : _set) {
+                    sum = s + n;
+                    if (sum % k == 0) {
+                        return false;
+                    }
                 }
+                return true;
             }
         }
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            if (sieve[i]) {
-                count++;
-            }
+
+        int getSize() {
+            return _set.size();
         }
-        return count;
+
+        void add(int n) {
+            _set.add(n);
+        }
     }
 
-    private void f1(int[] s, boolean[] cflags, int startIndex, int k) {
-        if (!cflags[startIndex])
-            return;
-        int i = 0;
-        boolean allSumsDividable = true;
-        while (i < s.length) {
-            if (i != startIndex) {
-                int sum = s[startIndex] + s[i];
-                boolean sumDividable = sum % k == 0;
-                cflags[i] = !sumDividable;
-                allSumsDividable &= sumDividable;
-            }
-            i++;
+    static int nonDivisibleSubset(int k, int[] s) {
+        int maxCardinality = 0;
+        int l = s.length;
+
+        for (int i = 0; i < l; i++) {
+            Nds nds = buildNds(s, i, k);
+            maxCardinality = Math.max(maxCardinality, nds.getSize());
         }
-
-        cflags[startIndex] = !allSumsDividable;
-
-        /*
-         * a questo punto l'array cflags vale true solo negli indici per i quali il
-         * corrispondente valore su S ha somma con S[startIndex] non divisibile per k
-         */
+        return maxCardinality;
     }
 
-    private int f2(int[] s, int k) {
-        /**
-         * ad ogni giro devi reimpostare l'array delle flag e dopo una chiamata a f1
-         * devi controllare quante flag sono rimaste alzate. Quel numero dovrebbe
-         * corrispondere alla cardinalità dell'insieme dei non-divisori.
-         * 
-         * trova poi la massima cardinalità, iniziando ogni volta da un elemento diverso
-         * dell'array di partenza
-         */
+    private static Nds buildNds(int[] s, int idx, int k) {
+        Nds nds = new Nds(k);
+        nds.add(s[idx]);
 
-        /**
-         * richiama f1() iniziando dal suo primo elemento, poi, dal secondo... infine
-         * dall'n-esimo: a fine procedimento conta quanti true sono rimasti nelle cflags
-         */
-
-        int maximalCardinality = 0;
-        for (int startIndex = 0; startIndex < s.length; startIndex++) {
-            boolean[] cflags = new boolean[s.length];
-            Arrays.fill(cflags, true);
-            f1(s, cflags, startIndex, k);
-            int cardinality = 0;
-            for (boolean b : cflags) {
-                if (b)
-                    ++cardinality;
+        int j = 0;
+        while (j < s.length) {
+            if (j != idx && nds.canBeAdded(s[j])) {
+                nds.add(s[j]);
             }
-            maximalCardinality = Math.max(maximalCardinality, cardinality);
-            if (maximalCardinality == 1) {
-                maximalCardinality = 0;
-            }
+            j++;
         }
-        return maximalCardinality;
-
+        return nds;
     }
 
     public static void main(String[] args) {
-        NonDivisibleSubset nds = new NonDivisibleSubset();
+        // NonDivisibleSubset nds = new NonDivisibleSubset();
         // int rank = nds.nonDivisibleSubset(3, new int[] { 1, 7, 2, 4 });
-        int rank = nds.f2(new int[] { 278, 576, 496, 727, 410, 124, 338, 149, 209, 702, 282, 718, 771, 575, 436 }, 7);
+        // int rank = nds.f2(new int[] { 278, 576, 496, 727, 410, 124, 338, 149, 209,
+        // 702, 282, 718, 771, 575, 436 }, 7);
+        int rank = NonDivisibleSubset.nonDivisibleSubset(7,
+                new int[] { 278, 576, 496, 727, 410, 124, 338, 149, 209, 702, 282, 718, 771, 575, 436 });
         System.out.println(rank);
 
     }
