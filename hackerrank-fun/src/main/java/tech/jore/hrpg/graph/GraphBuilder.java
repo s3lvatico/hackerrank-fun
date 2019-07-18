f034abec756613098e8dcf001de6b7b81bc65e7d
S3lvatico <s3lvatico@gmail.com>
2019-07-18 08:09:12 +0200

    uso try with resources

--- hackerrank-fun/src/main/java/tech/jore/hrpg/graph/GraphBuilder.java
+++ hackerrank-fun/src/main/java/tech/jore/hrpg/graph/GraphBuilder.java
@@ -14,20 +14,25 @@ public final class GraphBuilder {
     }
 
     public static Graph buildGraph(InputStream in) {
-        Scanner s = new Scanner(in);
-        int vertexes = s.nextInt();
+        try (Scanner s = new Scanner(in)) {
+            int vertexes = s.nextInt();
 
-        GraphImpl g = new GraphImpl(vertexes);
+            GraphImpl g = new GraphImpl(vertexes);
 
-        int edges = s.nextInt();
+            int edges = s.nextInt();
 
-        for (int i = 0; i < edges; i++) {
-            int v = s.nextInt();
-            int w = s.nextInt();
-            g.addEdge(v, w);
+            for (int i = 0; i < edges; i++) {
+                int v = s.nextInt();
+                int w = s.nextInt();
+                g.addEdge(v, w);
+            }
+
+            return g;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
         }
-
-        return g;
     }
 
 }
